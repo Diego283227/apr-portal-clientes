17acbf6053158ba1574f29a948e34cc5451081ca
Diego283227 <diegocalfuan@gmail.com>
2025-11-19 15:38:54 -0300

    Hide scrollbars in chat interface for cleaner look

--- portal-web/src/components/admin/ChatAdminView.tsx
+++ portal-web/src/components/admin/ChatAdminView.tsx
@@ -426,7 +426,7 @@ export default function ChatAdminView({ onBack }: ChatAdminViewProps) {
         </div>
 
         {/* Conversations List */}
-        <div className="flex-1 overflow-y-auto">
+        <div className="flex-1 overflow-y-auto scrollbar-hide">
           {loading ? (
             <div className="flex justify-center items-center h-32">
               <Loader2 className="w-6 h-6 animate-spin text-blue-500" />
@@ -561,7 +561,7 @@ export default function ChatAdminView({ onBack }: ChatAdminViewProps) {
             {/* Messages Area */}
             <div
               ref={messagesContainerRef}
-              className="flex-1 overflow-y-auto p-4 bg-gray-50"
+              className="flex-1 overflow-y-auto p-4 bg-gray-50 scrollbar-hide"
               style={{
                 backgroundImage: 'url("data:image/svg+xml,%3Csvg width="60" height="60" viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg"%3E%3Cg fill="none" fill-rule="evenodd"%3E%3Cg fill="%23f3f4f6" fill-opacity="0.3"%3E%3Cpath d="M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")'
               }}
@@ -770,7 +770,7 @@ export default function ChatAdminView({ onBack }: ChatAdminViewProps) {
           </div>
 
           {/* Profile Details */}
-          <div className="flex-1 overflow-y-auto p-4">
+          <div className="flex-1 overflow-y-auto p-4 scrollbar-hide">
             {loadingProfile ? (
               <div className="flex justify-center items-center h-32">
                 <Loader2 className="w-6 h-6 animate-spin text-blue-500" />
